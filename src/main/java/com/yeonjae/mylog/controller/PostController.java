8b63b8d51f4f3fed376813eade8a16725254c78b
yeonjaery <yeonjaery@google.com>
2022-11-09 02:05:35 +0900

    Feat: POST 데이터 콘텐츠 타입

--- src/main/java/com/yeonjae/mylog/controller/PostController.java
+++ src/main/java/com/yeonjae/mylog/controller/PostController.java
@@ -1,13 +1,23 @@
 package com.yeonjae.mylog.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yeonjae.mylog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
-    public String get() {
+    // Http Method
+    // GET POST PUT PATCH DELETE OPTIONS HEAD TRACE CONNECT
+    // 글 등록
+    // POST METHOD
+
+    @PostMapping("/posts")
+    public String post(@RequestBody PostCreate params) {
+        log.info("params={}", params.toString());
         return "Hello world";
     }
 }
