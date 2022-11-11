91d982a0e3f698aa67a07df601b3225c5f203449
YeonJae Jeong <hkzxcv12@naver.com>
2022-11-11 12:19:11 +0900

    Feat: 데이터 검증-2

--- src/main/java/com/yeonjae/mylog/controller/PostController.java
+++ src/main/java/com/yeonjae/mylog/controller/PostController.java
@@ -64,4 +64,20 @@ public class PostController {
         }
         return Map.of();
     }
+
+    // v2의 문제
+    /*
+        1. 매번 메서드마다 값을 검증 해야한다.
+            > 개발자가 까먹을 수 있다.
+            > 검증 부분에서 버그가 발생할 여지가 높다.
+            > 지겹다.
+        2. 응답값에 HashMap -> 응답 클래스를 만들어주는게 좋다.
+        3. 여러개의 에러처리는 힘듬
+        4. 세 번 이상의 반복적인 작업은 피해야한다.
+     */
+    @PostMapping("/v3/posts")
+    public Map<String, String> postV3(@RequestBody @Valid PostCreate params) throws Exception {
+
+        return Map.of();
+    }
 }
--- src/test/java/com/yeonjae/mylog/controller/PostControllerTest.java
+++ src/test/java/com/yeonjae/mylog/controller/PostControllerTest.java
@@ -7,11 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,6 +56,8 @@ class PostControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("{}"))
                 .andDo(print());
     }
+
+    // jsonPath: json 객체를 탐색하기 위한 표준화된 방법
     @Test
     @DisplayName("/v2/posts 요청시 title은 필수 값이다")
     void testV2Validation () throws Exception {
@@ -67,6 +66,34 @@ class PostControllerTest {
                         .content("{\"title\":\"\", \"content\" : \"내용입니다.\" }"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/v3/posts 요청시 Hello world를 출력한다.")
+    void testV3() throws Exception {
+
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.post("/v3/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\" }")
+                )
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("{}"))
+                .andDo(print());
+    }
+
+    // jsonPath: json 객체를 탐색하기 위한 표준화된 방법
+    @Test
+    @DisplayName("/v3/posts 요청시 title은 필수 값이다")
+    void testV3Validation () throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/v3/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"\", \"content\" : \"내용입니다.\" }"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
     }
 
